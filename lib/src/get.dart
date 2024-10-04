cbb73834b9a8dde8e81c2a6ec967c00f9c96a031
abdushakoor12 <abdushakoor009@yahoo.com>
2024-10-05 00:02:37 +0500

    move json parsing to a simple Isolate.run() that can be opted out

--- lib/src/get.dart
+++ lib/src/get.dart
@@ -1,29 +1,41 @@
 import "dart:convert";
+import "dart:isolate";
 
 import "package:http/http.dart" as http;
 
-Future<ApiResult> get(
-  Uri uri, {
-  Map<String, String>? queryParameters,
-}) async {
+Future<ApiResult> get(Uri uri,
+    {Map<String, String>? queryParameters,
+    bool useIsolateForParsing = true}) async {
   try {
     final response = await http.get(uri);
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> apiResponseData = jsonDecode(response.body);
-      if (apiResponseData["code"] == 200) {
-        final data = apiResponseData["data"];
-        if (data is List) {
-          return ApiSuccessList(data);
-        } else if (data is Map) {
-          return ApiSuccessMap(data as Map<String, dynamic>);
-        }
-      }
+
+    if (!useIsolateForParsing) {
+      return _parseResponse(response);
     }
-    return ApiError(response.body);
+
+    final apiResultFromIsolate = await Isolate.run(() async {
+      return _parseResponse(response);
+    });
+    return apiResultFromIsolate;
   } catch (e, trace) {
     print("Failed to get editions: $e $trace");
     return ApiError(e);
   }
+}
+
+Future<ApiResult> _parseResponse(http.Response response) async {
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> apiResponseData = jsonDecode(response.body);
+    if (apiResponseData["code"] == 200) {
+      final data = apiResponseData["data"];
+      if (data is List) {
+        return ApiSuccessList(data);
+      } else if (data is Map) {
+        return ApiSuccessMap(data as Map<String, dynamic>);
+      }
+    }
+  }
+  return ApiError(response.body);
 }
 
 sealed class ApiResult {}
